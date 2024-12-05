7c735f649b8c0d15740ae5430d3481b9622caedd
Bennett Dams <dams.bennett@gmail.com>
2024-12-05 01:36:46 +0100

    Add date utils & `DateTime` component

---
+++ src/components/date-time.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { DateTimeFormat, formatDateTime } from "@/lib/date-utils";
+import { useEffect, useState } from "react";
+
+/**
+ * Client component to render date-time. Avoids hydration mismatches.
+ */
+export function DateTime({
+  date,
+  format,
+  title,
+}: {
+  date: Date;
+  format: DateTimeFormat;
+  title?: string;
+}): JSX.Element {
+  // initialize with null to prevent server-render mismatches
+  const [dateTransformed, setDateTransformed] = useState<{
+    dateFormatted: string;
+    dateTime: string;
+  } | null>(null);
+
+  useEffect(() => {
+    setDateTransformed({
+      dateFormatted: formatDateTime(date, format),
+      dateTime: formatDateTime(date, "UTC YYYY-MM-DD"),
+    });
+  }, [date, format]);
+
+  return (
+    <time
+      dateTime={dateTransformed?.dateTime}
+      title={`${title ? title + " " : ""}${dateTransformed?.dateFormatted}`}
+    >
+      {dateTransformed?.dateFormatted}
+    </time>
+  );
+}
