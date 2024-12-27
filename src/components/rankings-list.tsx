d4df3af359f0309fe2c3d43ed8ba8be915457d81
Bennett Dams <dams.bennett@gmail.com>
2024-12-27 05:42:31 +0100

    Add ranking dialog for viewing/editing

--- src/components/rankings-list.tsx
+++ src/components/rankings-list.tsx
@@ -3,9 +3,20 @@ import type { Ranking } from "@/data/mock-data";
 import type { FiltersRankings } from "@/lib/schemas";
 import Image from "next/image";
 import { DateTime } from "./date-time";
-import { RankingsAdminControls } from "./rankings-admin-controls";
 import { RatingWithStars } from "./rating-with-stars";
+import { StarsForRating } from "./stars-for-rating";
+import { Button } from "./ui/button";
 import { Card, CardContent, CardFooter } from "./ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 import {
   Table,
   TableBody,
@@ -40,9 +51,6 @@ export async function RankingsList({
               <TableHead className="hidden md:table-cell">Note</TableHead>
               <TableHead className="hidden md:table-cell">
                 Reviewed at
-              </TableHead>
-              <TableHead>
-                <span className="sr-only">Actions</span>
               </TableHead>
             </TableRow>
           </TableHeader>
@@ -93,31 +101,94 @@ export function RankingsTableRow({
   restaurantName: Ranking["restaurantName"];
 }) {
   return (
-    <TableRow>
-      <TableCell className="min-w-14 p-0">
-        <Image
-          alt="Product image"
-          className="aspect-square rounded-md object-cover"
-          height="56"
-          src="/image-placeholder.svg"
-          width="56"
-        />
-      </TableCell>
-      <TableCell className="font-medium text-primary">
-        {restaurantName}
-      </TableCell>
-      <TableCell className="flex items-center font-medium">
-        <RatingWithStars rating={rating} />
-      </TableCell>
-      <TableCell className="font-medium">{product}</TableCell>
-      <TableCell className="font-medium">{category}</TableCell>
-      <TableCell className="font-medium">{note}</TableCell>
-      <TableCell className="hidden md:table-cell">
-        <DateTime format="YYYY-MM-DD" date={reviewedAt} />
-      </TableCell>
-      <TableCell>
-        <RankingsAdminControls />
-      </TableCell>
-    </TableRow>
+    <RankingDialog
+      restaurantName={restaurantName}
+      rating={rating}
+      product={product}
+      category={category}
+      note={note}
+      reviewedAt={reviewedAt}
+    >
+      <TableRow className="cursor-pointer">
+        <TableCell className="min-w-14 p-0">
+          <Image
+            alt="Product image"
+            className="aspect-square rounded-md object-cover"
+            height="56"
+            src="/image-placeholder.svg"
+            width="56"
+          />
+        </TableCell>
+        <TableCell className="font-medium text-primary">
+          {restaurantName}
+        </TableCell>
+        <TableCell className="flex items-center font-medium">
+          <RatingWithStars rating={rating} />
+        </TableCell>
+        <TableCell className="font-medium">{product}</TableCell>
+        <TableCell className="font-medium">{category}</TableCell>
+        <TableCell className="font-medium">{note}</TableCell>
+        <TableCell className="hidden md:table-cell">
+          <DateTime format="YYYY-MM-DD" date={reviewedAt} />
+        </TableCell>
+      </TableRow>
+    </RankingDialog>
+  );
+}
+
+function RankingDialog({
+  rating,
+  product,
+  category,
+  note,
+  reviewedAt,
+  restaurantName,
+  children,
+}: {
+  rating: Ranking["rating"];
+  product: Ranking["product"];
+  category: Ranking["category"];
+  note: Ranking["note"];
+  reviewedAt: Ranking["reviewedAt"];
+  restaurantName: Ranking["restaurantName"];
+  children: React.ReactNode;
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>
+            {restaurantName} - {product}
+          </DialogTitle>
+          <DialogDescription>{category}</DialogDescription>
+        </DialogHeader>
+
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <div>
+              <p className="text-center text-3xl">{rating}</p>
+              <StarsForRating rating={rating} />
+            </div>
+            <div>Note: {note}</div>
+            <div>
+              Last reviewed at:{" "}
+              <DateTime date={reviewedAt} format="YYYY-MM-DD" />
+            </div>
+          </div>
+        </div>
+
+        <DialogFooter className="sm:justify-end">
+          <Button type="button" variant="default">
+            Edit
+          </Button>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
