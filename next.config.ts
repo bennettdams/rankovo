60835f4a2a66c7b0213c861f01163c20c6514adb
Bennett Dams <dams.bennett@gmail.com>
2024-12-27 04:36:30 +0100

    Add API caching | Add Next.js logging

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
   experimental: {
     dynamicIO: true,
     ppr: true,
--- src/data/api.ts
+++ src/data/api.ts
@@ -1,7 +1,12 @@
 import type { FiltersRankings } from "@/lib/schemas";
+import { unstable_cacheTag as cacheTag } from "next/cache";
 import { createMockRankings } from "./mock-data";
 
 async function getRankings(filters: FiltersRankings) {
+  "use cache";
+  cacheTag("rankings");
+  console.debug("🟦 API rankings");
+
   await new Promise((r) => setTimeout(r, 1000));
 
   return createMockRankings(filters);
