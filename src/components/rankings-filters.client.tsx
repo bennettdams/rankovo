b41e5ea902af3d06c5ad3e62c96c3c3939b779c3
Bennett Dams <dams.bennett@gmail.com>
2025-01-10 00:52:23 +0100

    Add products & places tables

--- src/components/rankings-filters.client.tsx
+++ src/components/rankings-filters.client.tsx
@@ -86,6 +86,7 @@ function FiltersRankingsInternal({
         ratingMin: null,
         ratingMax: null,
       });
+
       router.push("/", { scroll: false });
     });
   }
--- src/db/db-schema.ts
+++ src/db/db-schema.ts
@@ -1,3 +1,4 @@
+import { categories } from "@/data/static";
 import {
   integer,
   pgTable,
@@ -11,6 +12,9 @@ export const reviewsTable = pgTable("reviews", {
   // 0 to 5 – Note: "real" is an inexact floating point number (e.g. has a problem with 0.1 + 0.2 = 0.30000000000000004)
   rating: real().notNull(),
   note: varchar({ length: 255 }),
+  productId: integer("product_id")
+    .references(() => productsTable.id)
+    .notNull(),
   reviewedAt: timestamp("reviewed_at", {
     precision: 6,
     withTimezone: true,
@@ -23,3 +27,25 @@ export type Review = typeof reviewsTable.$inferSelect;
 export type ReviewCreate = Required<typeof reviewsTable.$inferInsert>;
 export type ReviewUpdate = Partial<Review>;
 
+export const placesTable = pgTable("places", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at"),
+});
+
+export type Place = typeof placesTable.$inferSelect;
+
+export const productsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  note: varchar({ length: 255 }),
+  category: varchar({ length: 255, enum: categories }).notNull(),
+  placeId: integer("place_id")
+    .references(() => placesTable.id)
+    .notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at"),
+});
+
+export type Product = typeof productsTable.$inferSelect;
