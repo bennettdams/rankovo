9651806526bb94078e766c89c326bc70aa58e8bf
Bennett Dams <dams.bennett@gmail.com>
2025-01-12 01:14:27 +0100

    Add reviews page

---
+++ src/app/reviews/page.tsx
@@ -0,0 +1,32 @@
+import { queries } from "@/data/queries";
+import { Suspense } from "react";
+import { ReviewListItem } from "./review-list-item.client";
+
+export default async function PageReviews() {
+  return (
+    <div className="pt-8 md:pt-12">
+      <Suspense fallback={<div>Loading reviews...</div>}>
+        <ReviewsList />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ReviewsList() {
+  const reviews = await queries.reviews();
+
+  return (
+    <div className="grid gap-2 overflow-x-scroll">
+      {reviews.map((review) => (
+        <ReviewListItem key={review.id} review={review}>
+          <div>{review.id}</div>
+          <div>{review.rating}</div>
+          <div>created {review.createdAt.toISOString()}</div>
+          <div>reviewed {review.reviewedAt.toISOString()}</div>
+          <div>updated {review.updatedAt?.toISOString()}</div>
+          <div>{review.note}</div>
+        </ReviewListItem>
+      ))}
+    </div>
+  );
+}
---
+++ src/app/reviews/review-list-item.client.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { actionUpdateReview } from "@/data/actions";
+import { createRandomNumberBetween } from "@/data/mock-data";
+import { ratingHighest, ratingLowest } from "@/data/static";
+import type { Review } from "@/db/db-schema";
+import type { ReactNode } from "react";
+
+export function ReviewListItem({
+  review,
+  children,
+}: {
+  review: Review;
+  children: ReactNode;
+}) {
+  return (
+    <div
+      className="col-span-12 grid h-16 cursor-pointer grid-cols-subgrid items-center"
+      onClick={() =>
+        actionUpdateReview({
+          ...review,
+          rating: createRandomNumberBetween({
+            min: ratingLowest,
+            max: ratingHighest,
+            decimalPlaces: 1,
+          }),
+        })
+      }
+    >
+      {children}
+    </div>
+  );
+}
