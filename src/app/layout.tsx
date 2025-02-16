7cd362965a432529268b580c51cda353f665dd71
Bennett Dams <dams.bennett@gmail.com>
2025-02-16 03:49:39 +0100

    Add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/navbar";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Domine } from "next/font/google";
 import localFont from "next/font/local";
@@ -30,6 +31,7 @@ export default function RootLayout({
           fontSerif.variable,
         )}
       >
+        <Analytics />
         <Navbar />
 
         <main className="mb-52 min-h-full pb-40">
