bdc46d7876d05d94ede7fd1c807c8e0f112f1b2d
Bennett Dams <dams.bennett@gmail.com>
2025-04-07 00:30:31 +0200

    Fix join

--- src/data/queries.ts
+++ src/data/queries.ts
@@ -278,7 +278,7 @@ async function searchProduct({
     .from(productsTable)
     .where(and(...filtersSQL))
     // join is needed for filtering by place name (via filtersSQL)
-    .innerJoin(placesTable, eq(productsTable.placeId, placesTable.id))
+    .leftJoin(placesTable, eq(productsTable.placeId, placesTable.id))
     .as("queryProductsFiltered");
 
   /** 10 last reviews for each product */
