5f8280595ae03cd60d30d044df51dd2036d9b51e
Bennett Dams <dams.bennett@gmail.com>
2025-11-14 00:58:00 +0100

    Change position marker | Bump Next.js

--- src/components/ranking-position-marker.tsx
+++ src/components/ranking-position-marker.tsx
@@ -11,10 +11,15 @@ export function RankingPositionMarker({
   return (
     <div
       className={cn(
-        "mx-2 grid size-12 place-items-center rounded-full border-2 border-gray text-fg",
-        position === 1 && "border-none bg-[#FFD966] text-fg",
-        position === 2 && "border-none bg-[#B7CADB] text-fg",
-        position === 3 && "border-none bg-[#c27d6e] text-white",
+        "mx-2 grid size-12 place-items-center rounded-full text-fg",
+        position <= 3 && "shadow-lg",
+        position > 3 && "border-2 border-gray",
+        position === 1 &&
+          "bg-gradient-to-br from-yellow-500 via-yellow-400 to-yellow-600 text-white",
+        position === 2 &&
+          "bg-gradient-to-br from-slate-400 via-slate-300 to-slate-500 text-fg",
+        position === 3 &&
+          "bg-gradient-to-br from-amber-700 via-amber-600 to-amber-800 text-white",
       )}
     >
       {labelOverwrite ?? (
