4c17dd48a17c909e03d0ce2f3bb8e4fdc88f635a
Bennett Dams <dams.bennett@gmail.com>
2025-12-02 00:01:45 +0100

    WIP

--- src/app/unauthorized.tsx
+++ src/app/unauthorized.tsx
@@ -2,7 +2,7 @@ import { ErrorCard } from "@/components/error-card";
 
 export default function UnauthorizedPage() {
   return (
-    <ErrorCard title="Unauthorized">
+    <ErrorCard title="Nicht autorisiert">
       <p className="text-xl">Logge dich zuerst ein</p>
     </ErrorCard>
   );
