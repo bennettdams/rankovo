c92d26494fbdda4642f341e5c5013736b3d9e536
Bennett Dams <dams.bennett@gmail.com>
2025-12-21 22:35:06 +0100

    WIP

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -31,9 +31,9 @@ export default async function Image() {
         style={{ transform: "rotate(10deg)" }}
         fill="none"
         stroke="#eb8c21"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         aria-hidden="true"
       >
         <path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z"></path>
@@ -42,7 +42,6 @@ export default async function Image() {
       <div
         style={{
           backgroundImage: "linear-gradient(90deg, #eb8c21, #6c3e6e)",
-
           backgroundClip: "text",
           WebkitBackgroundClip: "text",
           color: "transparent",
